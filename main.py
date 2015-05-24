1cdfe0b8a7e3f5267df7d05a5c0bdc2d535edfbd
Toyz <toyzz@live.com>
2015-05-24 09:51:11 -0700

    Added super basic setup file right now

--- main.py
+++ main.py
@@ -12,20 +12,27 @@ def run(cflFile):
     tail = tail.split(".")[0]
     tail += ".chkn"
 
-    chkn = ChknFile.ChknFile(open("output/{0}".format(tail), "wb"), "w")
+    filename = "output/{0}".format(tail)
+    make_folder(filename)
+    chkn = ChknFile.ChknFile(open(filename, "wb"), "w")
 
     files = {}
     for name in cfl.getEntryNames():
         print "Converting: {0}".format(name)
         data = cfl.getContents(name)
         chkn.writestr(name, data)
+        make_folder("output/raw/{0}".format(name))
         open("output/raw/{0}".format(name), 'wb').write(data)
 
     print "Saved to \'output/{0}\'".format(tail)
     chkn.close()
 
+def make_folder(filename):
+    if not os.path.exists(os.path.dirname(filename)):
+        os.makedirs(os.path.dirname(filename))
+
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         run(sys.argv[1])
     else:
-        print "main.py CFLPath"
+        print "main.exe CFLPath"
