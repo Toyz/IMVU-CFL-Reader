f2248d4bee6515cd8c3695a037d022dfb0593c97
Toyz <toyzz@live.com>
2015-05-26 16:00:40 -0700

    Updated setup.py to support creating the UI.cfl

--- setup.py
+++ setup.py
@@ -2,10 +2,23 @@ __author__ = 'Toyz'
 
 from distutils.core import setup
 import py2exe
-
+from handlers.cfl.CFLMaker import CFLMaker
 from distutils.core import setup
 import py2exe
 import sys
+import os
+
+print "Building UI CFL file"
+cflMaker = CFLMaker("./interface/built/ui.cfl")
+
+uiLocation = "./interface"
+for i in os.listdir(uiLocation):
+    if os.path.isfile(os.path.join(uiLocation, i)):
+        f = open(os.path.join(uiLocation, i), "rb")
+        cflMaker.store(i, str(f.read()))
+        f.close()
+
+cflMaker.finish()
 
 sys.path.append("C:\\Windows\\WinSxS\\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.21022.8_none_bcb86ed6ac711f91")
 Mydata_files = [('.', ['.\\interface\\built\\ui.cfl']),
