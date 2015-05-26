c199196586c0071cc3c5b3e9bc8a050950aa0f22
Toyz <toyzz@live.com>
2015-05-26 16:13:02 -0700

    setup.py clean up

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 __author__ = 'Toyz'
 
-from distutils.core import setup
-import py2exe
 from handlers.cfl.CFLMaker import CFLMaker
 from distutils.core import setup
 import py2exe
@@ -21,7 +19,7 @@ for i in os.listdir(uiLocation):
 cflMaker.finish()
 
 sys.path.append("C:\\Windows\\WinSxS\\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.21022.8_none_bcb86ed6ac711f91")
-Mydata_files = [('.', ['.\\interface\\built\\ui.cfl']),
+data_files = [('.', ['.\\interface\\built\\ui.cfl']),
                 (".", ["C:\\Windows\\WinSxS\\x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.21022.8_none_bcb86ed6ac711f91\\msvcp90.dll"])]
 
-setup(windows=[{"script":"main_gui.py"}], options={"py2exe":{"includes":["sip"]}}, data_files=Mydata_files)
+setup(windows=[{"script":"main_gui.py"}], options={"py2exe":{"includes":["sip"]}}, data_files=data_files)
